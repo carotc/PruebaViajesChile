26cc8d9febad0f4f4ef60f8b9ae100cb23913c9f
carotc <c.tolosa@live.cl>
2024-04-22 18:01:49 -0400

    Avance3 se agregan funciones smooth scroll y css

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,5 +1,13 @@
-// Tooltip de bootstrap //
 
+// SCROLL NAV
+window.addEventListener("scroll", () => {
+    const nav = document.getElementById('nav');
+    (window.scrollY > 40)
+      ? nav.style.background = "#000"
+      : nav.style.background = "transparent"
+  })
+
+// Tooltip de bootstrap //
 $(document).ready(function () {
     $('[data-bs-toggle="tooltip"]').tooltip()
 })
